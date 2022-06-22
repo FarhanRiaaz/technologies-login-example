cd229a3106798fabab81f60d82e869db04c43159
farhan <farhaanwirk@gmail.com>
2022-06-22 20:21:38 +0500

    -added userViewModel to check if user already exits or not.

--- lib/domain/viewmodels/user_view_model.dart
+++ lib/domain/viewmodels/user_view_model.dart
@@ -1,23 +1,22 @@
 import 'package:technologies_login_example/di/components/service_locator.dart';
 import 'package:technologies_login_example/domain/enums/view_state.dart';
-import 'package:technologies_login_example/domain/models/login/login_request.dart';
+import 'package:technologies_login_example/domain/models/login/login_response.dart';
 import 'package:technologies_login_example/domain/repositories/user_repository.dart';
 import 'package:technologies_login_example/domain/viewmodels/base_view_model.dart';
 
-class LoginViewModel extends BaseViewModel {
+class UserViewModel extends BaseViewModel {
   final UserRepository _userRepository = getIt<UserRepository>();
-  String? errorMessage;
+  late User? user;
 
-  Future<bool> login(String email, String password) async {
+  Future<bool> isUserLoggedIn() async {
     setState(ViewState.BUSY);
-    var success = await _userRepository
-        .loginUser(LoginRequest(email: email, password: password));
+    var success = await _userRepository.getUser();
     setState(ViewState.IDLE);
-    if (success.data?.user?.id != null) {
-      _userRepository.insertUser(success.data!.user!);
+    if (success.first.id != null) {
+      user = success.first;
       return true;
     } else {
-      errorMessage = "Invalid credentials";
+      user = User();
       return false;
     }
   }
