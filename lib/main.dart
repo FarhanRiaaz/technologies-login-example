415c6d6866babf9b547f793ac304e00ef4214ee2
farhan <farhaanwirk@gmail.com>
2022-06-22 20:43:37 +0500

    -space fixes

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@ import 'package:technologies_login_example/pages/login_Screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
