e58c989be486caa8434cfe99353d02d086ab7981
farhan <farhaanwirk@gmail.com>
2022-06-23 10:52:47 +0500

    -updated description of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:technologies_login_example/di/components/service_locator.dart';
-import 'package:technologies_login_example/domain/viewmodel/base_view_model.dart';
 import 'package:technologies_login_example/pages/login_Screen.dart';
 
 Future<void> main() async {
@@ -16,7 +14,6 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
